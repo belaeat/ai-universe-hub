5da0eff18e1d57ef1618ac9cfca4b6efbea3f4c9
belaeat <omyhossain@gmail.com>
2023-03-04 02:29:40 +0600

    data loaded

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('live')
+
+const loadData = () =>{
+    const URL = 'https://openapi.programming-hero.com/api/ai/tools';
+    fetch (URL)
+    .then (res => res.json())
+    .then (data => console.log(data));
+}
+
+
+loadData();
